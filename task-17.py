5fec1d9dc7506883be9ad00f1d17a71611e8d640
adeebadel <adelarective@gmail.com>
2026-06-24 19:35:59 +0530

    task-17

---
+++ task-17.py
@@ -0,0 +1,27 @@
+def greet():
+    print("Hello Adeeb")
+greet()
+greet()
+greet()
+
+def train():
+    print("train hard")
+    print("stay consistent")
+train()
+
+def motivation():
+    print("Never give up")
+    print("Keep pushing forward")
+motivation()
+
+def goat():
+    print("The greatest of all time")
+    print("i am building my future")
+goat()
+
+def MMA():
+    print("punch")
+    print("kick")
+    print("elbow")
+    print("knee")
+MMA()

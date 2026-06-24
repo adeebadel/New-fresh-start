bc5acd5e91840d5b57b0e4fe8a2f3d68dfe4f959
adeebadel <adelarective@gmail.com>
2026-06-24 22:05:46 +0530

    task-20

--- task-19.py
+++ task-19.py
@@ -7,3 +7,4 @@ def greet(name):
 greet("Adeeb")
 greet("Sayera")
 greet("Zakeer")
+
---
+++ task-20.py
@@ -0,0 +1,18 @@
+def fighter(name):
+    print(name,"is training")
+fighter("Adeeb")
+fighter("Khabib")
+
+def square(num):
+    print(num*num)
+square(5)
+square(10)
+
+def future_age(age):
+    print("your future age will be",age+5)
+future_age(20)
+
+def add(a,b):
+    print(a+b)
+add(10,20)
+add(189,69)

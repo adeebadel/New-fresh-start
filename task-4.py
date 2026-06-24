c64c64edf0ba05f5fec2be1b2c1ab92ca495d6ad
adeebadel <adelarective@gmail.com>
2026-06-24 17:08:54 +0530

    task-4

---
+++ task-4.py
@@ -0,0 +1,15 @@
+name="Adeeb"
+age=20
+city="Bengaluru"
+print(name)
+print(age)
+print(city)
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+name = input("enter your name:")
+age = input("enter your age:")
+
+print("name",name)
+print("age",age)

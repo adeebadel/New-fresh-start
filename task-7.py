d3d8854ddc7ab60dbddd43037ab1f3c0799519ab
adeebadel <adelarective@gmail.com>
2026-06-24 17:31:06 +0530

    task-7

---
+++ task-7.py
@@ -0,0 +1,20 @@
+current_age = 20
+future_age = current_age + 5
+
+print("my current age is:", current_age)
+print("my age after 5 years will be : " ,future_age)
+
+
+num1 =int(input("enter the first number : "))
+num2 =int(input("enter the second number : "))
+print("sum=",num1+num2)
+print("difference=",num1-num2)
+print("product=",num1*num2)
+print("division=",num1/num2)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You are", age, "years old.")
+print("Next year, you will be", age + 1, "years old.")

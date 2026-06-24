b65dca6ca9b7bd90ba11a2932f236f3026cecf0d
adeebadel <adelarective@gmail.com>
2026-06-24 18:37:42 +0530

    task-10

---
+++ task-10.py
@@ -0,0 +1,20 @@
+age = int(input("Enter your age : "))
+
+if age>=18:
+    print("You can vote")
+else:
+    print("you can not vote")
+    
+num = int(input("Enter a number : "))
+
+if num> 0:
+    print("number is positive")
+else:
+    print("Number is zero or negative")
+    
+
+num = int(input("Enter a number : "))
+if num % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")

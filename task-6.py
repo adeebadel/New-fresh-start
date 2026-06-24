ea88c60e6391a5b8d62e8624c031c2870c35cf3e
adeebadel <adelarective@gmail.com>
2026-06-24 17:21:29 +0530

    task-6

---
+++ task-6.py
@@ -0,0 +1,15 @@
+name = "adeeb"
+age = 20
+Height = 5.11
+is_mma = True
+
+print(type(name))
+print(type(age))
+print(type(Height))
+print(type(is_mma))
+
+age = input("Enter your age : ")
+print(age+age)
+
+age = int(input("Enter your age : "))
+print(age+age)

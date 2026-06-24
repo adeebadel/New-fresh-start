fa8e3262cf8f928da5a640439dee4506c606f8d2
adeebadel <adelarective@gmail.com>
2026-06-24 19:29:54 +0530

    task-16

---
+++ task-16.py
@@ -0,0 +1,22 @@
+#while loops
+count = 1
+while count <= 5:
+    print(count)
+    count = count + 1
+    
+egg = 3
+
+while egg <= 5:
+    print(egg)
+    egg = egg + 1
+    
+banana = 2
+while banana <=8 :
+    print(banana)
+    banana = banana + 1
+    
+cow = 10
+
+while cow >= 2:
+    print(cow)
+    cow = cow - 2

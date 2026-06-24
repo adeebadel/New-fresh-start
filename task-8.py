703a6b43659d58aa1cf963d7fbd0cbb0995502b7
adeebadel <adelarective@gmail.com>
2026-06-24 18:20:14 +0530

    task-8

---
+++ task-8.py
@@ -0,0 +1,20 @@
+a = 10
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number :  "))
+
+print("addition=",num1+num2)
+print("subtraction=",num1-num2)
+print("multiplication=",num1*num2)
+print("division=",num1/num2)
+
+num = int(input("Enter a number : "))
+
+print("square=" ,num ** 2)
+print("cube=",num ** 3)

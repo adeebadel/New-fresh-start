e5b8901d064deaf722219de2d5bd0a00bcc6e84a
adeebadel <adelarective@gmail.com>
2026-06-24 18:41:36 +0530

    task-11

---
+++ task-11.py
@@ -0,0 +1,11 @@
+password = input("Enter your password: ")
+if password == "Goat123":
+    print("Access granted")
+else:
+    print("Access denied")
+    
+age = int(input("Enter your age : "))
+if age>16:
+    print("You can compete in MMA")
+else:
+    print("You can not compete in MMA")

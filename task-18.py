91b65788c24a91ab099e563fc7c921ef6775479b
adeebadel <adelarective@gmail.com>
2026-06-24 19:42:15 +0530

    task-18

---
+++ task-18.py
@@ -0,0 +1,22 @@
+def MMA():
+    print("kick")
+    print("punch")
+    print("elbow")
+    print("knee")
+    print("block")
+    print("dodge")
+    print("grapple")
+    print("submission")
+for _ in range(7):
+    MMA()
+    
+def motivation():
+    print("train hard")
+for _ in range(5):
+    motivation()
+    
+def hello():
+    print("hello")
+hello()
+print("world")
+hello()

6a74a0da749d66432b1395dcbb11ad53446b7458
adeebadel <adelarective@gmail.com>
2026-06-24 18:51:30 +0530

    task-13

--- task-13.py
+++ task-13.py
@@ -1,9 +1,9 @@
-for i in range(5):
+for _ in range(5):
     print("train")
     
-for i in range(1,6):
-    print(i)
+for _ in range(1,6):
+    print(_)
     
 num = int(input("Enter a number: "))
-for i in range(1,11):
-    print(num,"x",i,"=",num*i)
+for _ in range(1,11):
+    print(num,"x",_, "=", num*_)
